082dc5deb4ed9221c4b81adf0b0a3df60825c555
beikuncanada@gmail.com <1999fred>
2019-05-09 16:06:18 -0700

    implementation of ICU

--- src/InfiniteConvergentUniverse.java
+++ src/InfiniteConvergentUniverse.java
@@ -9,6 +9,7 @@ public class InfiniteConvergentUniverse {
     //private int numberOfNormalSubtrees;
     private int numberOfConvergentBrothers;
     private int numberOfBrothers;
+    private int numberOfSubtrees;
     private double parentNodePositionX;
     private double parentNodePositionY;
 
@@ -31,18 +32,23 @@ public class InfiniteConvergentUniverse {
     // total: tree height
     // node position: determine if the node is a leaf
     // convergence: a boolean to determine whether this node/branch should converge to the base world line
+    // number of brothers: shows how many subtrees does the parent node has , this number is used to generate the reqiured info to draw the graph
+    //                     note: info about subtrees are generated when the tree was first instantiated, the tree node cannot change how many brothers it has.
     // number of subtrees: a random number between 1 and 5
     //          indicates the number of subtrees of a node
     //          a node must have at least one subtree except for
-    // number of convergent subtrees, a number to determine number of convergent subtrees in this node,
+    // number of convergent brothers, a number to determine number of convergent subtrees in this node,
     //                  this number should be smaller than the number of subtrees, with a upper bound of 2
     // info about the parent node position to draw the graph
     // order: is the order of this node among all its brothers
-    public InfiniteConvergentUniverse(int total, int nodePosition, boolean convergence, int numberOfBrothers, int numberOfConvergentBrothers, double parentNodePositionX, double parentNodePositionY, int order){
+
+    //
+
+    public InfiniteConvergentUniverse(int total, int nodePosition, boolean convergence, int numberOfSubtrees, int numberOfBrothers, int numberOfConvergentBrothers, double parentNodePositionX, double parentNodePositionY, int order){
         this.total = total;
         this.nodePosition = nodePosition;
         this.convergence = convergence;
-        //this.numberOfNormalSubtrees = numberOfNormalSubtrees;
+        this.numberOfSubtrees = numberOfSubtrees;
         this.numberOfConvergentBrothers = numberOfConvergentBrothers;
         this.numberOfBrothers = numberOfBrothers;
         this.parentNodePositionX = parentNodePositionX;
@@ -71,13 +77,13 @@ public class InfiniteConvergentUniverse {
     public InfiniteConvergentUniverse generateANormalSubtree(int order){
         int numberOfSubs = 1+ (int) Math.floor(Math.random() * 4);
         int numberOfConvergentSubs = (int) Math.floor(Math.random()*2);
-        return new InfiniteConvergentUniverse(total, nodePosition+1, false, numberOfSubs, numberOfConvergentSubs, this.nodePositionX, nodePositionY, order);
+        return new InfiniteConvergentUniverse(total, nodePosition+1, false, numberOfSubs, this.numberOfSubtrees, numberOfConvergentSubs, this.nodePositionX, nodePositionY, order);
     }
 
     public InfiniteConvergentUniverse generateAConvergentSubtree(int order){
         int numberOfSubs = 1+ (int) Math.floor(Math.random() * 4);
         int numberOfConvergentSubs = (int) Math.floor(Math.random()*2);
-        return new InfiniteConvergentUniverse(total, nodePosition+1, true, numberOfSubs, numberOfConvergentSubs, this.nodePositionX, this.nodePositionY, order);
+        return new InfiniteConvergentUniverse(total, nodePosition+1, true, numberOfSubs, this.numberOfSubtrees, numberOfConvergentSubs, this.nodePositionX, this.nodePositionY, order);
     }
 
     public void generateGraphInfo(){
